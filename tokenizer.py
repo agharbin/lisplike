942bd2ba944fd0b2773dd1c21eaead15f1705a03
Adam Harbin <harbin.adam@gmail.com>
2016-03-13 19:25:32 -0500

    Simplifying tokenizer logic, adding strings

--- tokenizer.py
+++ tokenizer.py
@@ -6,6 +6,7 @@ import sys
 import sets
 
 needsValue = sets.Set(["ID","INT","FLOAT","STRING"])
+multiChar  = sets.Set(["ID","INT","FLOAT"])
 
 def isalpha(char):
     if ord(char) in range(ord('A'),ord('Z')): return True
@@ -16,69 +17,61 @@ def isnumeric(char):
     else: return False
 
 def tokenize_start(char,value):
-    if   char == "(": return ("LPAREN", "","")
-    elif char == ")": return ("RPAREN", "","")
+    if   char == "(": return ("START", "LPAREN","")
+    elif char == ")": return ("START", "RPAREN","")
+    elif char == "[": return ("START", "LBRACK","")
+    elif char == "]": return ("START", "RBRACK","")
+    elif char == "\"": return ("STRING", "", "")
     elif isalpha(char): return ("ID","",char)
     elif isnumeric(char): return ("NUM","",char)
     else: return ("START", "", "")
 
 def tokenize_id(char,value):
     if isalpha(char): return ("ID","",value + char)
-    elif char == "(": return ("LPAREN","ID",value)
-    elif char == ")": return ("RPAREN","ID",value)
-    else: return ("START", "ID", value) # End of input case
-
-def tokenize_lparen(char,value):
-    if isalpha(char): return ("ID","LPAREN",char)
-    elif char == "(": return ("LPAREN","LPAREN","")
-    elif char == ")": return ("RPAREN","LPAREN","")
-    else: return ("START","LPAREN","")
-
-def tokenize_rparen(char,value):
-    if isalpha(char): return ("ID","RPAREN",char)
-    elif char == "(": return ("LPAREN","RPAREN","")
-    elif char == ")": return ("RPAREN","RPAREN","")
-    else: return ("START","RPAREN","")
+    else: return ("START", "ID", value) 
 
 def tokenize_num(char, value):
     if isnumeric(char): return ("NUM","",value + char)
     elif char == ".": return ("FLOAT", "", value + char) 
-    elif char == "(": return ("LPAREN","INT",value)
-    elif char == ")": return ("RPAREN","INT",value)
     else: return ("START", "INT", value)
 
 def tokenize_float(char, value):
     if isnumeric(char): return ("FLOAT","",value + char)
-    elif char == "(": return ("LPAREN","FLOAT",value)
-    elif char == ")": return ("RPAREN","FLOAT",value)
     else: return ("START", "FLOAT", value)
+
+def tokenize_string(char,value):
+    if char == "\"": return ("START", "STRING", value)
+    else: return ("STRING", "", value + char)
+
+def emit_token(token,value):
+    if token != "":
+        if token in needsValue:
+            return (token,value)
+        else:
+            return (token,"")
 
 def tokenize(source):
     FSM = {
         "START"  : tokenize_start,
         "ID"     : tokenize_id,
-        "LPAREN" : tokenize_lparen,
-        "RPAREN" : tokenize_rparen,
         "NUM"    : tokenize_num,
-        "FLOAT"  : tokenize_float
+        "FLOAT"  : tokenize_float,
+        "STRING" : tokenize_string
     }
     state = "START"
     value = ""
     for char in source:
         state,token,value = FSM[state](char,value)
-        if token != "":
-            if token in needsValue:
-                yield (token,value)
-            else:
-                yield (token,"")
-
-    state,token,value = FSM[state](" ",value)
-    if token != "":
-        if token in needsValue:
-            yield (token,value)
-        else:
-            yield (token,"")
-
+        t = emit_token(token,value)
+        if t: yield t
+        if token in multiChar:
+            state,token,value = FSM["START"](char,"")
+            t = emit_token(token,value)
+            if t: yield t
+    # If input ends on a variable-length token, process a character to terminate
+    state,token,value = FSM[state]("\n",value)
+    t = emit_token(token,value)
+    if t: yield t
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
