6ea52eb4379519b6e987c15be6aa47e452db129c
Adam Harbin <harbin.adam@gmail.com>
2016-03-22 00:25:24 -0500

    Cleanup and adding primitive arithmetic expressions

--- parser.py
+++ parser.py
@@ -2,32 +2,74 @@ import tokenizer
 import sys
 
 token_stream = []
+next_token = []
+
+def get_next():
+    global token_stream
+    global next_token
+
+    try:
+        next_token = token_stream.next()
+    except StopIteration:
+        pass
 
 def apply_func(f, args):
+    # handle primitives
     if f == "+":
-        return int(args[0]) + int(args[1])
+        result = 0
+        for i in args:
+            result = result + i
+        return result
+    if f == "-":
+        return args[0] - args[1]
+    if f == "*":
+        result = 1
+        for i in args:
+            result = result * i
+        return result
+    if f == "/":
+        return args[0] / args[1]
 
 def parse_expression():
-    global token_stream
+    global next_token
 
-    token = token_stream.next()
-    if token[0] != "ID":
-        print "ERROR: expected identifier"
-    func = token[1]
-    args = []
-    while token[0] != "RPAREN":
-        token = token_stream.next()
-        args.append(token[1])
-    return apply_func(func, args)
+    if next_token[0] == "ID":
+        pass # unimplemented
+    elif next_token[0] == "INT":
+        result = int(next_token[1])
+        get_next()
+        return result
+    elif next_token[0] == "FLOAT":
+        result = float(next_token[1])
+        get_next()
+        return result
+    elif next_token[0] == "STRING":
+        result = next_token[1]
+        get_next()
+        return result
+    elif next_token[0] == "LPAREN":
+        get_next() # skip left parentheses
+    
+        if next_token[0] != "ID":
+            print "ERROR: expected identifier"
+        func = next_token[1]
+        get_next() # advance past function name
+
+        args = []
+        while next_token[0] != "RPAREN":
+            args.append(parse_expression())
+
+        get_next() # advance past right paren
+        return apply_func(func, args)
 
 def parse():
-    global token_stream
+    global next_token
     
-    token = token_stream.next()
-    if token[0] == "LPAREN":
-        return parse_expression()
-    elif token[0] == "LBRACK":
+    get_next()
+    if next_token[0] == "LBRACK":
         return parse_list()
+    else:
+        return parse_expression()
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
